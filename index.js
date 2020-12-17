7888aaec02958d8b2c8282c2df718b7f6398481e
danp20 <dan.parry22@gmail.com>
2020-12-17 17:44:35 +0100

    data storage

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// This line will handle all form parsing in the app automatically
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send(`
-        <div>
-            <form method="POST">
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <input name="passwordConfirmation" placeholder="password confirmation" />
-                <button>Sign Up</button>
-            </form>
-        </div>
-        `);
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
 });
 
-app.post("/", (req, res) => {
- console.log(req.body)
-    res.send("account created");
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
-    console.log("listening");   
-});     
+  console.log('Listening');
+});
--- repositories/users.js
+++ repositories/users.js
@@ -1,40 +1,47 @@
-const { Console } = require('console');
 const fs = require('fs');
 
 class UsersRepository {
     constructor(filename) {
-        if (!filename) {
-            throw new Error("Creating a repository requires a filename");
-        }
-
-        this.filename = filename;
-        try {
-            fs.accessSync(this.filename);
-        } catch (err) {
-            fs.writeFileSync(this.filename, '[]');
-            
-        }
+      if (!filename) {
+        throw new Error('Creating a repository requires a filename');
+      }
+  
+      this.filename = filename;
+      try {
+        fs.accessSync(this.filename);
+      } catch (err) {
+        fs.writeFileSync(this.filename, '[]');
+      }
     }
-
+  
     async getAll() {
-        // Open the file called this.filename
-        const contents = await fs.promises.readFile(this.filename, { 
-            encoding: 'utf8'
+      return JSON.parse(
+        await fs.promises.readFile(this.filename, {
+          encoding: 'utf8'
         })
-
-        // Read the content
-        console.log(contents);
-        // parse the conent 
-
-        // Return the parsed content
+      );
     }
- 
-}
 
-const test = async () => {
+    async create(attrs) {
+      const records = await this.getAll();
+      records.push(attrs)
+
+      await this.writeAll(records);
+    }
+
+    async writeAll(records) {
+      await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+  }
+  
+  const test = async () => {
     const repo = new UsersRepository('users.json');
+  
+    await repo.create({ email: "test@test.com", password: "test"})
 
-    await repo.getAll();
-};
+    const users = await repo.getAll();
 
-test();
+    console.log(users);
+  };
+  
+  test();
