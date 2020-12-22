775313f9087c9950e2580b5be8936515ab938542
danp20 <dan.parry22@gmail.com>
2020-12-22 15:41:26 +0100

    signup validation logic

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require("./repositories/users");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send(`
@@ -18,8 +20,18 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation} = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email })
+  if (existingUser) {
+    return res.send('Email address in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("passwords must match")
+  };
+
   res.send('Account created!!!');
 });
 
